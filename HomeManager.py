cac69410eccee8a60d51931254a292187706f346
darksv <darek969-12@o2.pl>
2016-02-28 13:02:43 +0100

    add resource for sensor

--- HomeManager.py
+++ HomeManager.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class SensorResource(Resource):
+    def get(self, sensor_id):
+        return {'sensor': sensor_id}
+
+    def put(self, sensor_id):
+        return {'sensor': sensor_id}
+
+api.add_resource(SensorResource, '/api/sensor/<sensor_id>')
 
 if __name__ == '__main__':
     app.run()
