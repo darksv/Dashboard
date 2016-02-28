0324be9b35314ea2cf62d35254a783de172a007f
darksv <darek969-12@o2.pl>
2016-02-28 15:46:16 +0100

    add API for sensor registration

--- HomeManager.py
+++ HomeManager.py
@@ -1,10 +1,25 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from lib.db import Database
+from lib.db.api import sensors as Sensors
 
 app = Flask(__name__)
 api = Api(app)
-db = Database()
+db = Database('mysql+pymysql://XXX:bUZ2so3KmBn43GqaIo1W@mysql8.mydevil.net/XXX')
+
+
+class SensorRegisterResource(Resource):
+    def put(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('internal_id', type=str)
+        parser.add_argument('type', type=int)
+        args = parser.parse_args()
+
+        sensor = Sensors.get_sensor_by_internal_id(db, args['internal_id'])
+        if sensor is None:
+            sensor = Sensors.create_sensor(db, args['internal_id'], args['type'])
+
+        return sensor._asdict()
 
 
 class SensorResource(Resource):
@@ -28,10 +43,10 @@ class ReadingResource(Resource):
 
         return parser.parse_args()
 
-
-api.add_resource(SensorListResource, '/api/sensors')
-api.add_resource(SensorResource, '/api/sensor/<sensor_id>')
-api.add_resource(ReadingResource, '/api/reading')
+api.add_resource(SensorRegisterResource, '/sensor/register')
+api.add_resource(SensorResource, '/sensor/<int:sensor_id>')
+api.add_resource(SensorListResource, '/sensors')
+api.add_resource(ReadingResource, '/reading')
 
 if __name__ == '__main__':
     app.run(debug=True)
