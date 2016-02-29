9055c87daf2666b15fae41d566142db7ad120051
darksv <darek969-12@o2.pl>
2016-02-29 21:49:02 +0100

    merge SensorsListResource into SensorsResource

--- HomeManager.py
+++ HomeManager.py
@@ -24,30 +24,45 @@ class SensorRegisterResource(Resource):
 
 
 class SensorResource(Resource):
-    def get(self, sensor_id):
-        sensor = Sensors.get_sensor(db, sensor_id)
-        if sensor is None:
+    def get(self, sensor_id=None):
+        multiple = sensor_id is None
+
+        if multiple:
+            sensors = Sensors.get_all_sensors(db)
+        else:
+            sensors = [Sensors.get_sensor(db, sensor_id)]
+
+        data = []
+
+        for sensor in sensors:
+            if sensor is None:
+                continue
+
+            sensor_data = sensor._asdict()
+
+            reading = Readings.get_last_reading(db, sensor.id)
+            if reading is not None:
+                last_update = reading.timestamp.timestamp()
+                last_value = reading.value
+            else:
+                last_update = None
+                last_value = None
+
+            sensor_data['updated'] = last_update
+            sensor_data['value'] = last_value
+
+            data.append(sensor_data)
+
+        if len(data) == 0:
             return {}
 
-        reading = Readings.get_last_reading(db, sensor.id)
-        if reading is not None:
-            reading_data = reading._asdict()
-            reading_data['timestamp'] = reading.timestamp.timestamp()
-        else:
-            reading_data = None
-
-        data = sensor._asdict()
-        data['reading'] = reading_data
+        if not multiple:
+            data = data[0]
 
         return dict(success=True, data=data, message=None)
 
     def put(self, sensor_id):
         return {'sensor': sensor_id}
-
-
-class SensorListResource(Resource):
-    def get(self):
-        return [sensor._asdict() for sensor in Sensors.get_all_sensors(db)]
 
 
 class ReadingResource(Resource):
@@ -71,8 +86,8 @@ class ReadingResource(Resource):
         return data
 
 # api.add_resource(SensorRegisterResource, '/sensors/register')
-api.add_resource(SensorListResource, '/sensors')
-api.add_resource(SensorResource, '/sensors/<int:sensor_id>')
+api.add_resource(SensorResource, '/sensors')
+api.add_resource(SensorResource, '/sensors/<int:sensor_id>', endpoint='sensors')
 api.add_resource(ReadingResource, '/readings')
 
 if __name__ == '__main__':
