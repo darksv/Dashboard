66e2a1a8e94a58ae72f912b3d20695094ea8a22e
darksv <darek969-12@o2.pl>
2016-03-04 20:27:18 +0100

    add function for datetime to localized ISO8601 format

--- app/util.py
+++ app/util.py
@@ -7,3 +7,10 @@ def localize_datetime(dt: datetime) -> datetime:
     Add local timezone to datetime.
     """
     return get_localzone().localize(dt)
+
+
+def convert_datetime_to_iso(dt: datetime) -> str:
+    """
+    Convert datetime to ISO8601 string.
+    """
+    return localize_datetime(dt).isoformat()
