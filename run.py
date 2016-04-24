e48fdffe12d037868dc173790c6105ca531c6c06
darksv <darek969-12@o2.pl>
2016-04-24 22:18:42 +0200

    add subpage for sensor details

--- run.py
+++ run.py
@@ -14,6 +14,10 @@ if config.DEVELOPMENT:
     def send_js(path: str):
         return send_from_directory('app/static/js', path)
 
+    @app.route('/fonts/<path:path>')
+    def send_font(path: str):
+        return send_from_directory('app/static/fonts', path)
+
 
 @app.route('/')
 def hello_world():
@@ -21,5 +25,12 @@ def hello_world():
 
     return render_template('index.html', title='Dashboard', sensors=result.json()['data'])
 
+
+@app.route('/sensor/<int:sensor_id>')
+def sensor_details(sensor_id: int):
+    result = requests.get('http://test.hsdxd.usermd.net/sensors/{0}'.format(sensor_id))
+
+    return render_template('sensor_datails.html', title='Czujniczeq' + str(sensor_id), sensor=result.json()['data'])
+
 if __name__ == '__main__':
-    app.run(debug=config.DEVELOPMENT)
+    app.run(debug=config.DEVELOPMENT, host=config.HOST)
