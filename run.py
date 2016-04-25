ada9edeeb4e88dc437f8cddc0f573a0d77979ac6
darksv <darek969-12@o2.pl>
2016-04-25 21:31:48 +0200

    remove some code

--- run.py
+++ run.py
@@ -37,15 +37,8 @@ def api_redirect(path: str):
 @app.route('/sensor/<int:sensor_id>')
 def sensor_details(sensor_id: int):
     sensor_data = requests.get('http://test.hsdxd.usermd.net/sensors/{0}'.format(sensor_id)).json()['data']
-    sensor_stats_daily = requests.get('http://test.hsdxd.usermd.net/sensors/{0}/stats/daily'.format(sensor_id)).json()['data']
 
-    stats_daily = collections.OrderedDict(reversed(sensor_stats_daily))
-    stats_daily_hours = json.dumps(list(stats_daily.keys()))
-    stats_daily_values = json.dumps(list(stats_daily.values()))
-
-    return render_template('sensor_datails.html', title='Czujniczeq' + str(sensor_id),
-                           sensor=sensor_data,
-                           stats_daily=(stats_daily_hours, stats_daily_values))
+    return render_template('sensor_datails.html', title='Czujniczeq' + str(sensor_id), sensor=sensor_data)
 
 if __name__ == '__main__':
     app.run(debug=config.DEVELOPMENT, host=config.HOST)
