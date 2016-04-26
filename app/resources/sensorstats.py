91cecba977cf0190b3688d2bf43f82b8f3e71f34
darksv <darek969-12@o2.pl>
2016-04-26 15:29:15 +0200

    add monthly stats

--- app/resources/sensorstats.py
+++ app/resources/sensorstats.py
@@ -5,6 +5,11 @@ from flask_restful import Resource
 
 class SensorStatsResource(Resource):
     def get(self, sensor_id, period):
-        data = Stats.get_daily_stats(DB, sensor_id)
+        data = []
+
+        if period == 'daily':
+            data = Stats.get_daily_stats(DB, sensor_id)
+        elif period == 'monthly':
+            data = Stats.get_monthly_stats(DB, sensor_id)
 
         return dict(success=True, data=data, message=None)
