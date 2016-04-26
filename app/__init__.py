e72051b027eb0cb705da7029645493cf16e97b58
darksv <darek969-12@o2.pl>
2016-04-26 19:38:04 +0200

    rename /sensors to /channels

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from app.resources.entry import EntryResource
 app = Flask(__name__)
 
 api = Api(app)
-api.add_resource(ChannelResource, '/sensors')
-api.add_resource(ChannelResource, '/sensors/<int:channel_id>', endpoint='sensors')
-api.add_resource(ChannelStatsResource, '/sensors/<int:channel_id>/stats/<string:period>')
+api.add_resource(ChannelResource, '/channels')
+api.add_resource(ChannelResource, '/channels/<int:channel_id>', endpoint='channels')
+api.add_resource(ChannelStatsResource, '/channels/<int:channel_id>/stats/<string:period>')
 api.add_resource(EntryResource, '/updates')
