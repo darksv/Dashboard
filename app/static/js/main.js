64c01bf82bf68d8df7a6fd67b9ac88de8783e632
darksv <darek969-12@o2.pl>
2016-04-26 22:48:14 +0200

    add some realtime chart

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -41,6 +41,75 @@ function createChart(container, title)
     return new Highcharts.Chart(options);
 }
 
+function createRealtimeChart(container, channelId)
+{
+    var options = {
+        chart: {
+            type: 'spline',
+            animation: Highcharts.svg,
+            events: {
+                load: function () {
+                    var series = this.series[0];
+
+                    setInterval(function(){
+                        $.getJSON('/api/channels/' + channelId, function(response) {
+                            var data = response.data,
+                                x = (new Date(data.value_updated)).getTime(),
+                                y = data.value,
+                                lastTimestamp = series.data[series.data.length - 1].x;
+
+                            if (lastTimestamp != x)
+                                series.addPoint([x, y], true, true);
+                        });
+                    }, 5000);
+                }
+            },
+            renderTo: container
+        },
+        title: {
+            text: 'Wykres w czasie rzeczywistym'
+        },
+        xAxis: {
+            type: 'datetime',
+            tickPixelInterval: 150
+        },
+        yAxis: {
+            title: {
+                text: 'Temperatura (°C)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            enabled: false
+        },
+        legend: {
+            enabled: false
+        },
+        exporting: {
+            enabled: false
+        },
+        series: [{}]
+    };
+
+    $.getJSON('/api/channels/' + channelId + '/stats/recent', function(response) {
+        var data = response.data
+            points = [];
+
+        for (var i = 0; i < data.length; ++i)
+        {
+            var entry = data[i];
+            points.push([(new Date(entry[0])).getTime(), entry[1]]);
+        }
+
+        options.series[0].data = points;
+        new Highcharts.Chart(options);
+    });
+}
+
 function updateChart(chart, type, sensorId)
 {
     $.getJSON('/api/channels/' + sensorId + '/stats/' + type, function(data) {
@@ -72,13 +141,24 @@ function updateChart(chart, type, sensorId)
 
 $(function () {
     const chartTitles = {
+        realtime: 'Na żywo',
         daily: 'Ostatnie 24 godziny',
         monthly: 'Ostatni miesiąc'
     };
 
     $('[data-chart]').each(function() {
         var chartType = $(this).data('chart');
-        var chart = createChart(this, chartTitles[chartType]);
-        updateChart(chart, chartType, $(this).data('sensor-id'));
+        var sensorId = $(this).data('sensor-id')
+        var chart = null;
+
+        if (chartType == 'realtime')
+        {
+            chart = createRealtimeChart(this, sensorId);
+        }
+        else
+        {
+            chart = createChart(this, chartTitles[chartType]);
+            updateChart(chart, chartType, sensorId);
+        }
     });
 });
