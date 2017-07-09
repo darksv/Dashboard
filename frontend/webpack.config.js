eeb8b0dbd32f07b9dd1d7314cbc67930181e358f
darksv <darek969-12@o2.pl>
2017-07-09 13:52:16 +0200

    Splitted js into chunks

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,8 +1,13 @@
-var path = require('path');
+const path = require('path');
+const webpack = require('webpack');
+const DefinePlugin = webpack.DefinePlugin
+const CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: {
-        'bundle': './src/index.js'
+        'app': './src/index.js',
+        'vendor': ['vue', 'vue-router', 'vuedraggable', 'axios', 'chart.js', 'mqtt']
     },
     devtool: "source-map",
     output: {
@@ -16,5 +21,20 @@ module.exports = {
                 loader: 'vue-loader',
             }
         ]
-    }
+    },
+    plugins: [
+        new CommonsChunkPlugin({
+			names: ['vendor'],
+			minChunks: Infinity
+		}),
+        new DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new UglifyJSPlugin({
+            include: /\.min\.js$/,
+            minimize: true
+        })
+    ]
 };
