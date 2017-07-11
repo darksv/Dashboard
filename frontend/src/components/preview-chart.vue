5f5c857eb98658f9dc8de1c0878cddf8704084c3
darksv <darek969-12@o2.pl>
2017-07-11 07:57:00 +0200

    Fixed preview chart

--- frontend/src/components/preview-chart.vue
+++ frontend/src/components/preview-chart.vue
@@ -23,67 +23,71 @@
                 default: 0
             }
         },
-        watch: {
-            'channel.items': function () {
-                var items = clampArray(this.channel.items, this.maxPoints);
-
-                this.chart.data.datasets[0].data = items.map(function(x) {
-                    return x[1];
-                });
-
-                this.chart.data.labels = items.map(function(x) {
+        computed: {
+            config: function () {
+                return {
+                    type: 'line',
+                    data: {
+                        labels: this.labels,
+                        datasets: [
+                            {
+                                fill: false,
+                                lineTension: 0,
+                                pointRadius: 0,
+                                data: this.values,
+                                borderWidth: 2.5,
+                                borderColor: this.color,
+                                label: ''
+                            }
+                        ]
+                    },
+                    options: {
+                        animation: false,
+                        responsive: false,
+                        maintainAspectRatio: false,
+                        tooltips: {
+                            enabled: false
+                        },
+                        hover: {
+                            mode: null
+                        },
+                        legend: {
+                            display: false
+                        },
+                        scales: {
+                            yAxes: [{
+                                display: false
+                            }],
+                            xAxes: [{
+                                display: false
+                            }],
+                        },
+                        title: {
+                            display: false
+                        }
+                    }
+                };
+            },
+            labels: function() {
+                return clampArray(this.channel.items, this.maxPoints).map(function(x) {
                     return x[0];
                 });
-
-                this.chart.update();
             },
-            color: function () {
-                this.chart.data.datasets[0].borderColor = this.color;
+            values: function() {
+                return clampArray(this.channel.items, this.maxPoints).map(function(x) {
+                    return x[1];
+                });
+            }
+        },
+        watch: {
+            config: function(config) {
+                this.chart.config.data = config.data;
+                this.chart.config.options = config.options;
                 this.chart.update();
             }
         },
         mounted: function () {
-            this.chart = new Chart(this.$el, {
-                type: 'line',
-                data: {
-                    labels: [],
-                    datasets: [
-                        {
-                            fill: false,
-                            lineTension: 0,
-                            pointRadius: 0,
-                            borderColor: this.color,
-                            data: this.values,
-                            label: ''
-                        }
-                    ]
-                },
-                options: {
-                    animation: false,
-                    responsive: false,
-                    maintainAspectRatio: false,
-                    tooltips: {
-                        enabled: false
-                    },
-                    hover: {
-                        mode: null
-                    },
-                    legend: {
-                        display: false
-                    },
-                    scales: {
-                        xAxes: [{
-                            display: false
-                        }],
-                        yAxes: [{
-                            display: false
-                        }]
-                    },
-                    title: {
-                        display: false
-                    }
-                }
-            });
+            this.chart = new Chart(this.$el, this.config);
         }
     };
 </script>
