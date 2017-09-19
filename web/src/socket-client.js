bc8ae57274badf4e30efb125a9d56f2b5cee4439
darksv <darek969-12@o2.pl>
2017-09-19 21:40:44 +0200

    Automatic reconnect to websocket server

--- web/src/socket-client.js
+++ web/src/socket-client.js
@@ -1,12 +1,33 @@
 class SocketClient {
     constructor(url, app) {
+        let self = this;
+        this._url = url;
         this._handlers = {};
+        this._onConnected = () => app.connected = true;
+        this._onDisconnected = () => {
+            app.connected = false;
+            setTimeout(() => self._reconnect(), 1000);
+        };
+        this._reconnect();
+    }
+
+    _reconnect() {
+        try {
+            this._connect();
+        } catch (e) {
+            this._onDisconnected();
+            console.error(e);
+        }
+    }
+
+    _connect() {
         let self = this,
-            socket = new WebSocket(url);
-        socket.onopen = e => app.connected = true;
-        socket.onclose = e => app.connected = false;
-        socket.onerror = e => console.log(e);
-        socket.onmessage = e => {
+            socket = new WebSocket(this._url);
+
+        socket.addEventListener('open', e => self._onConnected());
+        socket.addEventListener('close', e => self._onDisconnected());
+        socket.addEventListener('error', e => console.error(e));
+        socket.addEventListener('message', e => {
             let message = JSON.parse(e.data),
                 name = message[0],
                 data = message[1];
@@ -16,17 +37,19 @@ class SocketClient {
                     handler(data);
                 }
             } else {
-               console.log('unhandled', name);
+               console.warn('unhandled', name);
             }
-        };
+        });
         this._socket = socket;
     }
+
     addEventListener(event, handler) {
         if (!(event in this._handlers)) {
             this._handlers[event] = [];
         }
         this._handlers[event].push(handler);
     }
+
     removeEventListener(event, handler) {
         if (!(event in this._handlers)) {
             return;
@@ -35,6 +58,7 @@ class SocketClient {
             index = handlers.indexOf(handler);
         this._handlers[event] = handlers.splice(index, 1);
     }
+
     send(action, data) {
         this._socket.send(JSON.stringify([action, data]));
     }
