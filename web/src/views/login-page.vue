5c393b75fa319ce51b1bb96f8e506efbd212f05f
darksv <darek969-12@o2.pl>
2017-09-21 18:45:27 +0200

    Added 'remember me' checkbox in login form

--- web/src/views/login-page.vue
+++ web/src/views/login-page.vue
@@ -1,21 +1,25 @@
 <template>
     <div class="login-page">
-        <form v-if="state == 'default'" class="login-form" method="POST" @submit.prevent="login">
-            <div>
-                <input type="text" class="text-input" name="username" placeholder="Username" required="required">
-            </div>
-            <div>
-                <input type="password" class="text-input" name="password" placeholder="Password" required="required">
-            </div>
-            <div>
-                <input type="submit" class="button" value="Login">
-            </div>
-        </form>
-        <loader v-if="state == 'logging'"></loader>
-        <p v-if="state == 'failed'" class="error-message">
+        <loader v-if="state === 'logging'"></loader>
+        <p v-else-if="state === 'failed'" class="error-message">
             Wrong username and/or password.<br>
             Please <a @click="state='default'">try again</a>!
         </p>
+        <form v-else class="login-form" method="POST" @submit.prevent="submit">
+            <div>
+                <input type="text" class="text-input" placeholder="Username" required="required" v-model="username">
+            </div>
+            <div>
+                <input type="password" class="text-input" placeholder="Password" required="required" v-model="password">
+            </div>
+            <div>
+                <label>
+                    <input type="checkbox" v-model="remember">
+                    Remember me
+                </label>
+                <input type="submit" class="button" value="Login">
+            </div>
+        </form>
     </div>
 </template>
 <script>
@@ -27,17 +31,18 @@
             return {
                 username: '',
                 password: '',
+                remember: false,
                 state: 'default'
             };
         },
         methods: {
-            login() {
+            submit() {
                 if (this.state !== 'default') {
                     return;
                 }
-                let self = this;
-                self.state = 'logging';
-                login(this.username, this.password, () => self.state = 'success', () => self.state = 'failed');
+
+                this.state = 'logging';
+                login(this.username, this.password, () => this.state = 'success', () => this.state = 'failed');
             },
             clear() {
                 this.username = '';
@@ -59,9 +64,24 @@
 
     .login-form {
         max-width: 600px;
+
         div {
             padding: 0.1em;
             margin: 0.75em 0;
+
+            &:last-child {
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+
+                :nth-child(1) {
+                    flex: auto;
+                }
+
+                :nth-child(2) {
+                    flex: 1;
+                }
+            }
         }
     }
 
