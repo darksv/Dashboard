c288e6ea729a36d056c941752b81ee477786785d
darksv <darek969-12@o2.pl>
2017-09-23 22:08:32 +0200

    Minor code changes.

--- web/src/views/channel-custom-page.vue
+++ web/src/views/channel-custom-page.vue
@@ -2,9 +2,9 @@
     <div class="channel-custom-page">
         <div class="chart-toolbar">
             <div class="chart-toolbar-fields" v-if="fieldsShown">
-                <input type="date" class="input" v-model="from" v-on:keyup.enter="show" :readonly="isLoading">
+                <input type="date" class="input" v-model="from" v-on:keyup.enter="show" :readonly="isLoading" title="Start">
                 -
-                <input type="date" class="input" v-model="to" v-on:keyup.enter="show" :readonly="isLoading">
+                <input type="date" class="input" v-model="to" v-on:keyup.enter="show" :readonly="isLoading" title="End">
             </div>
             <span class="fa fa-calendar chart-toolbar-button" role="button" v-on:click.prevent="toggleFields"></span>
         </div>
@@ -17,6 +17,7 @@
     import Chart from '../components/chart.vue';
     import Loader from '../components/loader.vue';
     import { client as ApiClient } from '../api-client.js';
+    import {zip} from "../functional";
 
     function isValidDate(str) {
         return !isNaN(Date.parse(str));
@@ -26,14 +27,18 @@
         return new Date(this.getTime() + n * 24 * 3600 * 1000);
     };
 
+    Date.prototype.toShort = function () {
+        return this.toISOString().substr(0, 10);
+    };
+
     export default {
-        data: function () {
+        data() {
             return {
                 points: [],
                 title: '',
                 unit: '',
-                from: '',
-                to: '',
+                from: (new Date).addDays(-30).toShort(),
+                to: (new Date).toShort(),
                 fieldsShown: false,
                 isLoading: true,
                 maxPoints: 60
@@ -45,59 +50,56 @@
             }
         },
         computed: {
-            formattedFrom: function () {
+            fromForUrl() {
                 return this.from.replace(/-/g, '') + '0000';
             },
-            formattedTo: function () {
+            toForUrl() {
                 return this.to.replace(/-/g, '') + '2359';
             }
         },
         watch: {
-            channel: function() {
+            channel() {
                 this.update();
             },
-            from: function () {
+            from() {
                 this.periodChanged = true;
             },
-            to: function () {
+            to() {
                 this.periodChanged = true;
             }
         },
-        created: function () {
-            var dateFrom, dateTo;
-
-            if (isValidDate(this.$route.query.from) && isValidDate(this.$route.query.to)) {
-                dateFrom = new Date(this.$route.query.from);
-                dateTo = new Date(this.$route.query.to);
-            } else {
-                dateFrom = (new Date).addDays(-30);
-                dateTo = (new Date);
+        created() {
+            let query = this.$route.query;
+            if (isValidDate(query.from) && isValidDate(query.to)) {
+                this.from = new Date(query.from).toShort();
+                this.to = new Date(query.to).toShort();
             }
 
             this.periodChanged = true;
-            this.from = dateFrom.toISOString().substr(0, 10);
-            this.to = dateTo.toISOString().substr(0, 10);
             this.show();
         },
         methods: {
-            update: function() {
-                var url = '/channel/' + this.channel.id + '/stats?type=custom&from=' + this.formattedFrom + '&to=' + this.formattedTo;
-                var self = this;
-                self.fieldsEnabled = false;
-                self.isLoading = true;
-                ApiClient.get(url).then(function (response) {
-                    var data = response.data;
-                    self.points = [];
-                    for (var i = 0; i < data.labels.length; ++i) {
-                        self.points.push([data.labels[i], data.values[i]]);
-                    }
-                    self.title = data.title;
-                    self.unit = data.unit;
-                    self.fieldsEnabled = true;
-                    self.isLoading = false;
-                });
+            update() {
+                let url = '/channel/' + this.channel.id + '/stats',
+                    options = {
+                        params: {
+                            type: 'custom',
+                            from: this.fromForUrl,
+                            to: this.toForUrl,
+                            average: 1440
+                        }
+                    };
+
+                this.isLoading = true;
+                ApiClient.get(url, options).then(response => {
+                    let data = response.data;
+                    this.points = zip(data.labels, data.values);
+                    this.title = data.title;
+                    this.unit = data.unit;
+                    this.isLoading = false;
+                }).catch(() => this.isLoading = false);
             },
-            show: function () {
+            show() {
                 if (this.periodChanged !== true) {
                     return;
                 }
@@ -115,7 +117,7 @@
                     this.update();
                 }
             },
-            toggleFields: function () {
+            toggleFields() {
                 this.fieldsShown = !this.fieldsShown;
 
                 if (!this.fieldsShown) {
@@ -124,8 +126,8 @@
             }
         },
         components: {
-            Chart: Chart,
-            Loader: Loader
+            Chart,
+            Loader
         }
     };
 </script>
